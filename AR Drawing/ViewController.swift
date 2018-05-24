1e2a653f05626f0f3fafb41ea6bf36c987ce2105
devbaggett <devabaggett@gmail.com>
2018-05-24 16:26:40 -0700

    got current position of camera

--- AR Drawing/ViewController.swift
+++ AR Drawing/ViewController.swift
@@ -29,11 +29,23 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    // delegate function that helps us draw. gets called every time view is about to render a scene; neverending loop
+    // delegate function that helps us draw. gets called every time view is about to render a scene; neverending loop; gets called 60x/second
     func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
-        
+        guard let pointOfView = sceneView.pointOfView else {return}
+        let transform = pointOfView.transform
+        // get orientation of camera
+        let orientation = SCNVector3(-transform.m31,-transform.m32,-transform.m33)
+        // current location of camera; 3d vector xyz
+        let location = SCNVector3(transform.m41,transform.m42,transform.m43)
+        // get position from location and orientation
+        let currentPositionOfCamera = orientation + location
+        print(orientation.x, orientation.y, orientation.z)
     }
 
 
+}
+
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
 }
 
